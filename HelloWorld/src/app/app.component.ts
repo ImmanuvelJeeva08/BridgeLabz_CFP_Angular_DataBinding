c50d8550048b06977d5da0273b9ea526a30d9528
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-09-28 10:13:17 -0700

    To display Hello from BridgeLabz Using oneWay Data Binding

---
+++ HelloWorld/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  /**
+   * Declaring the title variable in string format (Typescript-way) and assigning value to it.
+   */
+  
+   title: string = 'hello-world';
+
+   ngOnInit(): void {
+    /**
+     * Overriding the above value of title variable and assigning a new value to the same variable.
+     */
+    this.title = "Hello from BridgeLabz";
+  } 
+
+}
+
+
