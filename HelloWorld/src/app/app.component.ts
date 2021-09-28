16a3b0055c3076306467e41a0b90f471315481d6
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-09-28 10:32:51 -0700

    To display BridgeLabz Logo using property Binding Technique

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -12,14 +12,25 @@ export class AppComponent {
    */
   
    title: string = 'hello-world';
+   imgUrl: string;
+  
+   /**
+     * Method with no return type.
+     * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. 
+     * Called only once in the lifecycle.
+     */
 
    ngOnInit(): void {
     /**
      * Overriding the above value of title variable and assigning a new value to the same variable.
      */
     this.title = "Hello from BridgeLabz";
-  } 
 
+     /**
+     * Assigning value to the imgUrl variable to implement the concept of Property Binding.
+     */
+      this.imgUrl = "../assets/BL_logo_square_jpg.jpg";
+    } 
 }
 
 
